e53f45c59f5143ff04a86ae2adbcb5b1b4c8c477
Theo <theopremartin@orange.fr>
2024-11-16 14:18:24 +0100

    localisation front changement

--- src/app/components/ProgressBar.js
+++ src/app/components/ProgressBar.js
@@ -4,25 +4,28 @@ import Image from 'next/image';
 
 const ProgressBar = ({ currentStep }) => {
     return (
-        <div className="flex my-5">
+        <div className="flex justify-between items-center my-5">
             <Image
                 src="/check-out.png"
                 alt="order food"
                 width={50}
                 height={50}
             />
+            <span className='text-black text-3xl'> ➔ </span>
             <Image
                 src="/cooking.png"
                 alt="cooking"
                 width={50}
                 height={50}
             />
+            <span className='text-black text-3xl'> ➔ </span>
             <Image
                 src="/fast-delivery.png"
                 alt="delivery"
                 width={50}
                 height={50}
             />
+            <span className='text-black text-3xl'> ➔ </span>
             <Image
                 src="/delivery-man.png"
                 alt="order delivered"
